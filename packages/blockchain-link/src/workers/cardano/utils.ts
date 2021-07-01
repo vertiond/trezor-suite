616b054e4b938874369bc1f95d3f6b52075a91b6
Vladimir Volek <volek.vladimir@gmail.com>
2021-07-01 15:55:32 +0200

    fix: transactions

--- packages/blockchain-link/src/workers/cardano/utils.ts
+++ packages/blockchain-link/src/workers/cardano/utils.ts
@@ -1,7 +1,9 @@
 import { BlockfrostUtxos, BlockfrostTransaction, BlockfrostAccountInfo } from '../../types/cardano';
 import { Utxo } from '../../types/responses';
 import BigNumber from 'bignumber.js';
-import { Transaction, AccountInfo, AccountAddresses, TokenInfo } from '../../types/common';
+import { Address } from '../../types';
+import { VinVout } from '../../types/blockbook';
+import { Transaction, AccountInfo, AccountAddresses, TokenInfo, Target } from '../../types/common';
 
 export const transformUtxos = (utxos: BlockfrostUtxos[]): Utxo[] => {
     const result: Utxo[] = [];
@@ -25,10 +27,7 @@ export const transformUtxos = (utxos: BlockfrostUtxos[]): Utxo[] => {
     return result;
 };
 
-const getTxType = (
-    blockfrostTxData: BlockfrostTransaction,
-    accountAddress?: AccountAddresses
-): 'self' | 'recv' | 'sent' => 'self';
+type Addresses = (Address | string)[] | string;
 
 export const transformTokenInfo = (
     tokens: BlockfrostAccountInfo['tokens']
@@ -51,25 +50,119 @@ export const transformTokenInfo = (
     return info.length > 0 ? info : undefined;
 };
 
+export const filterTargets = (addresses: Addresses, targets: VinVout[]): VinVout[] => {
+    if (typeof addresses === 'string') {
+        addresses = [addresses];
+    }
+    // neither addresses or targets are missing
+    if (!addresses || !Array.isArray(addresses) || !targets || !Array.isArray(targets)) return [];
+
+    const all: (string | null)[] = addresses.map(a => {
+        if (typeof a === 'string') return a;
+        if (typeof a === 'object' && typeof a.address === 'string') return a.address;
+        return null;
+    });
+
+    return targets.filter(t => {
+        if (t && Array.isArray(t.addresses)) {
+            return t.addresses.filter(a => all.indexOf(a) >= 0).length > 0;
+        }
+        return false;
+    });
+};
+
+export const transformInputOutput = (
+    data: BlockfrostTransaction['txUtxos']['inputs'] | BlockfrostTransaction['txUtxos']['outputs']
+): Target[] =>
+    data.map((utxo, i) => ({
+        n: i,
+        addresses: [utxo.address],
+        isAddress: true,
+        amount: utxo.amount.find(a => a.unit === 'lovelace')?.quantity ?? '0',
+    }));
+
+const sumVinVout = (
+    vinVout: VinVout[],
+    initialValue = '0',
+    operation: 'sum' | 'reduce' = 'sum'
+) => {
+    const sum = vinVout.reduce((bn, v) => {
+        if (typeof v.value !== 'string') return bn;
+        return operation === 'sum' ? bn.plus(v.value) : bn.minus(v.value);
+    }, new BigNumber(initialValue));
+    return sum.toString();
+};
+
 export const transformTransaction = (
     descriptor: string,
     accountAddress: AccountAddresses | undefined,
     blockfrostTxData: BlockfrostTransaction
 ): Transaction => {
-    const lovelaceBalance = blockfrostTxData.txData.output_amount.find(b => b.unit === 'lovelace');
+    const myAddresses = accountAddress
+        ? accountAddress.change.concat(accountAddress.used, accountAddress.unused)
+        : [descriptor];
+
+    let type: Transaction['type'];
+    let targets: VinVout[] = [];
+    let amount =
+        blockfrostTxData.txData.output_amount.find(b => b.unit === 'lovelace')?.quantity || '0';
     const fee = blockfrostTxData.txData.fees;
-    const totalSpent = new BigNumber(lovelaceBalance?.quantity || '0').plus(fee).toString();
+    let totalSpent = new BigNumber(amount || '0').plus(fee).toString();
+    const inputs = transformInputOutput(blockfrostTxData.txUtxos.inputs);
+    const outputs = transformInputOutput(blockfrostTxData.txUtxos.outputs);
+    const vinLength = Array.isArray(inputs) ? inputs.length : 0;
+    const voutLength = Array.isArray(outputs) ? outputs.length : 0;
+    const outgoing = filterTargets(myAddresses, inputs);
+    const incoming = filterTargets(myAddresses, outputs);
+    const internal = accountAddress ? filterTargets(accountAddress.change, outputs) : [];
+
+    if (outgoing.length === 0 && incoming.length === 0) {
+        type = 'unknown';
+    } else if (
+        vinLength > 0 &&
+        voutLength > 0 &&
+        outgoing.length === vinLength &&
+        incoming.length === voutLength
+    ) {
+        // all inputs and outputs are mine
+        type = 'self';
+        targets = outputs.filter(o => internal.indexOf(o) < 0);
+        // recalculate amount, amount spent is just a fee
+        amount = blockfrostTxData.txData.fees;
+        totalSpent = amount;
+    } else if (outgoing.length === 0 && incoming.length > 0) {
+        // none of the input is mine but and output or token transfer is mine
+        type = 'recv';
+        amount = '0';
+        if (incoming.length > 0) {
+            targets = incoming;
+            // recalculate amount, sum all incoming vout
+            amount = sumVinVout(incoming, amount);
+            totalSpent = amount;
+        }
+    } else {
+        type = 'sent';
+        // regular targets
+        if (voutLength) {
+            // bitcoin-like transaction
+            // sum all my inputs
+            const myInputsSum = sumVinVout(outgoing, '0');
+            // reduce sum by my outputs values
+            totalSpent = sumVinVout(incoming, myInputsSum, 'reduce');
+            amount = new BigNumber(totalSpent).minus(fee ?? '0').toString();
+        }
+    }
 
     return {
-        type: getTxType(blockfrostTxData, accountAddress),
+        type,
         txid: blockfrostTxData.txHash,
         blockTime: blockfrostTxData.blockInfo.time,
         blockHeight: blockfrostTxData.blockInfo.height || undefined,
         blockHash: blockfrostTxData.blockInfo.hash,
-        amount: lovelaceBalance?.quantity || '0',
+        amount,
         fee,
         totalSpent,
-        targets: [],
+        targets,
         tokens: [],
         details: {
             vin: [],
@@ -90,10 +183,7 @@ export const transformAccountInfo = (info: BlockfrostAccountInfo): AccountInfo =
         history: {
             transactions: !cardanoTxs
                 ? []
-                : cardanoTxs.map((tx: any) =>
-                      // @ts-ignore
-                      transformTransaction(info.descriptor, info.addresses, tx)
-                  ),
+                : cardanoTxs.map(tx => transformTransaction(info.descriptor, info.addresses, tx)),
         },
     };
 
