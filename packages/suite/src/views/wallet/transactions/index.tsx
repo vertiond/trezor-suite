ac7f3fee781cf3be6b2331f56abab4485f14857c
Vladimir Volek <volek.vladimir@gmail.com>
2021-11-29 15:28:58 +0100

    fix: hide tx summary for cardano

--- packages/suite/src/views/wallet/transactions/index.tsx
+++ packages/suite/src/views/wallet/transactions/index.tsx
@@ -31,9 +31,8 @@ const Content = ({
             account={selectedAccount}
             showEmptyHeaderPlaceholder={showEmptyHeaderPlaceholder}
         >
-            {showSummary && account.networkType !== 'ripple' && (
-                <TransactionSummary account={account} />
-            )}
+            {(showSummary && account.networkType !== 'ripple') ||
+                (account.networkType !== 'cardano' && <TransactionSummary account={account} />)}
             {children}
         </WalletLayout>
     );
